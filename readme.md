68eedf0e7012d274bb243a167dff2726badb3d3a
Chinedu Orie <nedsoftdeveloper@gmail.com>
2022-02-20 14:00:14 +0000

    test: add unit and integration tests

---
+++ readme.md
@@ -0,0 +1,103 @@
+# Api Docs
+
+## BASE_URL:
+
+
+
+> [Link to the API documentation]('#)
+
+# Endpoints Summary
+
+| Verb | Route      | Description | Auth Required |
+| ---- | ---------- | ----------- | ------------- |
+| GET  | `/`        | Index Route | false         |
+| POST | `/records` | Create cake | false         |
+
+## [POST] `/records` Fetch records from the database
+
+#### Sample Request Payload
+
+```json
+{
+  "startDate": "2016-01-26",
+  "endDate": "2018-02-02",
+  "minCount": 2700,
+  "maxCount": 3000
+}
+```
+
+#### Sample Response Payload
+
+```json
+{
+  "code": 0,
+  "msg": "Success",
+  "records": [
+    {
+      "key": "ibfRLaFT",
+      "createdAt": "2016-12-25T16:43:27.909Z",
+      "totalCount": 2892
+    },
+    {
+      "key": "pxClAvll",
+      "createdAt": "2016-12-19T10:00:40.050Z",
+      "totalCount": 2772
+    }
+  ]
+}
+```
+
+#### Response Payload interpretation
+
+```
+code = 0 msg = "Success" imply successful request
+code = 422 implies invalid request payload input format
+code = 505 implies server error
+code = 404 implies Resource Not Found / invalid endpoint
+
+```
+
+# Usage
+
+## Requirements
+
+> Node installed
+
+> NPM or Yarn install
+
+#### Steps
+
+- `git clone https://github.com/nedssoft/cake-crud.git && cd cake-crud`
+- `touch .env && cp .env.example .env`
+- `npm install`
+- `npm run dev`
+
+### Fetch Records
+
+```curl
+curl -X POST -H "Content-Type: application/json" \
+    -d '{"startDate": "2016-01-26", "endDate": "2018-02-02", "minCount": 2700, "maxCount": 3000}' \
+    http://localhost:3000/api/records
+```
+
+## Test
+Test cases are located in the tests director
+To run the automated test, run the following command
+
+> npm test
+
+
+# Framework/libraries
+
+- [Express](https://expressjs.com/)
+
+# Test libraries
+
+- [Jest](https://jestjs.io/docs)
+- [Supertest](https://www.npmjs.com/package/supertest)
+
+
+
+# Licence
+
+- MIT
--- src/tests/records.spec.js
+++ src/tests/records.spec.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const server = require('../server');
 const getCollection = require('../db');
+const res = require('express/lib/response');
 
 describe('DB Collection', () => {
   it('should create connection to the mongodb server', async () => {
@@ -28,16 +29,49 @@ describe('RECORDS', () => {
     );
   });
 
-  it('should return records', async () => {
-    const response = await request(server).post('/api/records').send({
-      startDate: '2016-01-26',
-      endDate: '2018-02-02',
+  it('should return empty records if request payload is out of rage', async () => {
+    const payload = {
+      startDate: '1515-01-26',
+      endDate: '1616-02-02',
       minCount: 2700,
       maxCount: 3000,
-    });
+    };
+    const response = await request(server).post('/api/records').send(payload);
     expect(response.statusCode).toEqual(200);
     expect(response.body.code).toEqual(0);
     expect(response.body.msg).toEqual('Success');
     expect(response.body).toHaveProperty('records');
+    expect(response.body.records).toHaveLength(0);
+  })
+  it('should return records matching sample response payload', async () => {
+    const payload = {
+      startDate: '2016-01-26',
+      endDate: '2018-02-02',
+      minCount: 2700,
+      maxCount: 3000,
+    };
+    const response = await request(server).post('/api/records').send(payload);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.code).toEqual(0);
+    expect(response.body.msg).toEqual('Success');
+    expect(response.body).toHaveProperty('records');
+    response.body.records.forEach((record) => {
+      expect(record).toStrictEqual(
+        expect.objectContaining({
+          key: expect.any(String),
+          createdAt: expect.any(String),
+          totalCount: expect.any(Number),
+        }),
+      );
+
+      expect(new Date(record.createdAt).getTime()).toBeGreaterThanOrEqual(
+        new Date(payload.startDate).getTime(),
+      );
+      expect(new Date(record.createdAt).getTime()).toBeLessThanOrEqual(
+        new Date(payload.endDate).getTime(),
+      );
+      expect(record.totalCount).toBeGreaterThanOrEqual(payload.minCount);
+      expect(record.totalCount).toBeLessThanOrEqual(payload.maxCount);
+    });
   });
 });
